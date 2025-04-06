2e745388c39c8057c094e9cc872ea79b10b2344a
Nathalia Soares <nathalia.oliveira24@fatec.sp.gov.br>
2025-04-05 23:10:12 -0300

    Including CORS

--- main.go
+++ main.go
@@ -5,10 +5,14 @@ import (
 	"net/http"
 
 	"github.com/Nathalia-Soares/sql_injection/controller"
+	"github.com/Nathalia-Soares/sql_injection/middlewares/cors"
 )
 
 func main() {
 	controller.SetupController()
+
+	handler := cors.EnableCORS(http.DefaultServeMux)
+
 	log.Println("Servidor rodando na porta 8080")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", handler)
 }
